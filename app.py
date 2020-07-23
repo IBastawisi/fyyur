e4ba34e9b93a9fb58a23e454e2895d18d9149098
= <=>
2020-07-23 05:27:58 +0200

    fix nested query

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import dateutil.parser
 from datetime import datetime
 import babel
+import collections
 from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort, jsonify
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -15,191 +16,9 @@ import sys
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
-
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
-
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-Migrate(app, db)
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
-
-class Show(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime)
-
-    venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey(
-        "artist.id"), nullable=False)
-
-    venue = db.relationship("Venue", back_populates="shows")
-    artist = db.relationship("Artist", back_populates="shows")
-
-    def __repr__(self):
-        return f'{self.id}: {self.name}'
-
-
-class Venue(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    genres = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean())
-    seeking_description = db.Column(db.String(500))
-    shows = db.relationship(
-        "Show", cascade="all,delete", back_populates="venue")
-
-    def __repr__(self):
-        return f'{self.id}: {self.name}'
-
-
-class Artist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean())
-    seeking_description = db.Column(db.String(500))
-    shows = db.relationship("Show", cascade="all,delete",
-                            back_populates="artist")
-
-    def __repr__(self):
-        return f'{self.id}: {self.name}'
-
-
-#----------------------------------------------------------------------------#
-# Initial Data.
-#----------------------------------------------------------------------------#
-db.drop_all()
-db.create_all()
-
-artist1 = Artist(
-    name="Guns N Petals",
-    genres="Rock n Roll",
-    city="San Francisco",
-    state="CA",
-    phone="326-123-5000",
-    website="https://www.gunsnpetalsband.com",
-    facebook_link="https://www.facebook.com/GunsNPetals",
-    seeking_venue=True,
-    seeking_description="Looking for shows to perform at in the San Francisco Bay Area!",
-    image_link="https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-)
-
-artist2 = Artist(
-    name="Matt Quevedo",
-    genres="Jazz",
-    city="New York",
-    state="NY",
-    phone="300-400-5000",
-    facebook_link="https://www.facebook.com/mattquevedo923251523",
-    seeking_venue=False,
-    image_link="https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-)
-
-artist3 = Artist(
-    name="The Wild Sax Band",
-    genres="Jazz,Classical",
-    city="San Francisco",
-    state="CA",
-    phone="432-325-5432",
-    seeking_venue=False,
-    image_link="https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-)
-
-venue1 = Venue(
-    name="The Musical Hop",
-    genres="Jazz,Reggae,Swing,Classical,Folk",
-    address="1015 Folsom Street",
-    city="San Francisco",
-    state="CA",
-    phone="123-123-1234",
-    website="https://www.themusicalhop.com",
-    facebook_link="https://www.facebook.com/TheMusicalHop",
-    seeking_talent=True,
-    seeking_description="We are on the lookout for a local artist to play every two weeks. Please call us.",
-    image_link="https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-)
-
-venue2 = Venue(
-    name="The Dueling Pianos Bar",
-    genres="Classical,R&B,Hip-Hop",
-    address="335 Delancey Street",
-    city="New York",
-    state="NY",
-    phone="914-003-1132",
-    website="https://www.theduelingpianos.com",
-    facebook_link="https://www.facebook.com/theduelingpianos",
-    seeking_talent=False,
-    image_link="https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-)
-
-venue3 = Venue(
-    name="Park Square Live Music & Coffee",
-    genres="Rock n Roll,Jazz,Classical,Folk",
-    address="34 Whiskey Moore Ave",
-    city="San Francisco",
-    state="CA",
-    phone="415-000-1234",
-    website="https://www.parksquarelivemusicandcoffee.com",
-    facebook_link="https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-    seeking_talent=False,
-    image_link="https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-)
-
-show1 = Show(
-    venue_id=1,
-    artist_id=1,
-    start_time="2019-05-21T21:30:00.000Z"
-)
-
-show2 = Show(
-    venue_id=3,
-    artist_id=2,
-    start_time="2019-06-15T23:00:00.000Z"
-)
-
-show3 = Show(
-    venue_id=3,
-    artist_id=3,
-    start_time="2035-04-01T20:00:00.000Z"
-)
-
-show4 = Show(
-    venue_id=3,
-    artist_id=3,
-    start_time="2035-05-01T20:00:00.000Z"
-)
-
-show5 = Show(
-    venue_id=3,
-    artist_id=3,
-    start_time="2035-06-01T20:00:00.000Z"
-)
-
-db.session.add_all([artist1, artist2, artist3, venue1, venue2,
-                    venue3, show1, show2, show3, show4, show5])
-db.session.commit()
+from models import *
+from config import db, app
+from init import *
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -237,17 +56,21 @@ def index():
 
 @app.route('/venues')
 def venues():
-    areas = Venue.query.with_entities(Venue.city, Venue.state).group_by(Venue.city, Venue.state).all()
-    upcoming_shows = Show.query.with_entities(Show.venue_id, db.func.count(
-        "*").label("num_upcoming_shows")).filter(Show.start_time > datetime.today()).group_by(Show.venue_id).subquery()
+    areas = Venue.query.with_entities(
+        Venue.city, Venue.state).group_by(Venue.city, Venue.state).all()
+
+    upcoming_shows = Show.query.with_entities(Show.venue_id, db.func.count("*").label("num_upcoming_shows"))\
+        .filter(Show.start_time > datetime.today()).group_by(Show.venue_id).subquery()
+
+    venues = Venue.query.with_entities(Venue.id, Venue.name, Venue.city, Venue.state, upcoming_shows.c.num_upcoming_shows)\
+        .outerjoin(upcoming_shows, upcoming_shows.c.venue_id == Venue.id).all()
 
     data = []
     for area in areas:
-        venues=Venue.query.filter(Venue.city == area.city).with_entities(Venue.id, Venue.name, upcoming_shows.c.num_upcoming_shows).outerjoin(upcoming_shows, upcoming_shows.c.venue_id == Venue.id).all()
         data.append({
             "city": area.city,
             "state": area.state,
-            "venues": venues
+            "venues": list(filter(lambda x: x.city == area.city, venues))
         })
 
     return render_template('pages/venues.html', areas=data)
@@ -256,8 +79,8 @@ def venues():
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
 
-    venues = Venue.query.with_entities(Venue.id, Venue.name).filter(Venue.name.ilike(
-        f"%{request.form.get('search_term', '')}%")).all()
+    venues = Venue.query.with_entities(Venue.id, Venue.name)\
+        .filter(Venue.name.ilike(f"%{request.form.get('search_term', '')}%")).all()
 
     response = {'count': len(venues), 'data': venues}
 
@@ -270,33 +93,24 @@ def show_venue(venue_id):
     venue = Venue.query.get(venue_id)
     upcoming_shows = []
     past_shows = []
-    
+
     for show in venue.shows:
-        result = {"artist_id":show.artist_id,"artist_name":show.artist.name,"artist_image_link":show.artist.image_link,"start_time":show.start_time}
-        if show.start_time > datetime.today() :
+        result = {"artist_id": show.artist_id, "artist_name": show.artist.name,
+                  "artist_image_link": show.artist.image_link, "start_time": show.start_time}
+        if show.start_time > datetime.today():
             upcoming_shows.append(result)
         else:
             past_shows.append(result)
 
-    data = {
-        "id": venue.id,
-        "name": venue.name,
-        "genres": venue.genres.split(","),
-        "address": venue.address,
-        "city": venue.city,
-        "state": venue.state,
-        "phone": venue.phone,
-        "website": venue.website,
-        "facebook_link": venue.facebook_link,
-        "image_link": venue.image_link,
-        "seeking_talent": venue.seeking_talent,
-        "seeking_description": venue.seeking_description,
+    data = venue.__dict__
 
-        "upcoming_shows": upcoming_shows,
-        "past_shows": past_shows,
-        "upcoming_shows_count": len(upcoming_shows),
-        "past_shows_count": len(past_shows)
-    }
+    data["genres"] = venue.genres.split(",")
+
+    data['past_shows'] = past_shows
+    data['past_shows_count'] = len(past_shows)
+
+    data['upcoming_shows'] = upcoming_shows
+    data['upcoming_shows_count'] = len(upcoming_shows)
 
     return render_template('pages/show_venue.html', venue=data)
 
@@ -402,32 +216,24 @@ def show_artist(artist_id):
     artist = Artist.query.get(artist_id)
     upcoming_shows = []
     past_shows = []
-    
+
     for show in artist.shows:
-        result = {"venue_id":show.venue_id,"venue_name":show.venue.name,"venue_image_link":show.venue.image_link,"start_time":show.start_time}
-        if show.start_time > datetime.today() :
+        result = {"venue_id": show.venue_id, "venue_name": show.venue.name,
+                  "venue_image_link": show.venue.image_link, "start_time": show.start_time}
+        if show.start_time > datetime.today():
             upcoming_shows.append(result)
         else:
             past_shows.append(result)
 
-    data = {
-        "id": artist.id,
-        "name": artist.name,
-        "genres": artist.genres.split(","),
-        "city": artist.city,
-        "state": artist.state,
-        "phone": artist.phone,
-        "website": artist.website,
-        "facebook_link": artist.facebook_link,
-        "image_link": artist.image_link,
-        "seeking_venue": artist.seeking_venue,
-        "seeking_description": artist.seeking_description,
+    data = artist.__dict__
 
-        "upcoming_shows": upcoming_shows,
-        "past_shows": past_shows,
-        "upcoming_shows_count": len(upcoming_shows),
-        "past_shows_count": len(past_shows)
-    }
+    data["genres"] = artist.genres.split(",")
+
+    data['past_shows'] = past_shows
+    data['past_shows_count'] = len(past_shows)
+
+    data['upcoming_shows'] = upcoming_shows
+    data['upcoming_shows_count'] = len(upcoming_shows)
 
     return render_template('pages/show_artist.html', artist=data)
 
@@ -440,7 +246,7 @@ def edit_artist(artist_id):
     artist = Artist.query.get(artist_id)
     form = ArtistForm(obj=artist)
     form.genres.data = artist.genres.split(",")
-    
+
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+from flask import Flask
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,6 +13,10 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgres:///fyyur'
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+db = SQLAlchemy(app)
